3de2b38cdc703460afe6e01ba876dcdf4b02f2cc
Nick Colley <nickcolley7@gmail.com>
2017-12-05 01:23:31 +0000

    Set default app port

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ nunjucks.configure(['public'], {
   express: app
 });
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
