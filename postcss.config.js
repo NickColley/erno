9bfd5a5f9f259e7de3577e3537f40952f2ad7e76
Nick Colley <nickcolley7@gmail.com>
2017-12-29 13:32:02 +0000

    Filter unwanted PostCSS warnings

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,14 @@ module.exports = {
       'black': BLACK
     },
     'postcss-cssnext': {},
-    'cssnano': { autoprefixer: false }
+    'cssnano': { autoprefixer: false },
+    'postcss-reporter': {
+      clearAllMessages: true,
+      filter: function (message) {
+        const isUnitConverstion = message.text === 'Unit cannot be used for conversion, so 16px is used.';
+        const isDiscardEmpty = message.plugin === 'postcss-discard-empty'
+        return !isUnitConverstion && !isDiscardEmpty;
+      }
+    }
   }
 }
